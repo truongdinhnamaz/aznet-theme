cc9d240b2773220d866ffc05f88038e20dc097d9
truongdinhnamaz <truongnam151@gmail.com>
2026-09-03 12:07:58 +0700

    refactor: keep W2 asset change minimal

--- inc/theme/assets.php
+++ inc/theme/assets.php
@@ -14,16 +14,49 @@ if ( ! defined( 'ABSPATH' ) ) {
 function enqueue_assets(): void {
     $version = defined( 'AZNET_THEME_VERSION' ) ? AZNET_THEME_VERSION : null;
 
-    wp_enqueue_style( 'aznet-theme-tokens', get_theme_file_uri( '/assets/css/tokens.css' ), [], $version );
-    wp_enqueue_style( 'aznet-theme-style', get_stylesheet_uri(), [ 'aznet-theme-tokens' ], $version );
-    wp_enqueue_style( 'aznet-theme-site-header', get_theme_file_uri( '/assets/css/components/site-header.css' ), [ 'aznet-theme-tokens' ], $version );
-    wp_enqueue_style( 'aznet-theme-site-footer', get_theme_file_uri( '/assets/css/components/site-footer.css' ), [ 'aznet-theme-tokens' ], $version );
+    wp_enqueue_style(
+        'aznet-theme-tokens',
+        get_theme_file_uri( '/assets/css/tokens.css' ),
+        [],
+        $version
+    );
+
+    wp_enqueue_style(
+        'aznet-theme-style',
+        get_stylesheet_uri(),
+        [ 'aznet-theme-tokens' ],
+        $version
+    );
+
+    wp_enqueue_style(
+        'aznet-theme-site-header',
+        get_theme_file_uri( '/assets/css/components/site-header.css' ),
+        [ 'aznet-theme-tokens' ],
+        $version
+    );
+
+    wp_enqueue_style(
+        'aznet-theme-site-footer',
+        get_theme_file_uri( '/assets/css/components/site-footer.css' ),
+        [ 'aznet-theme-tokens' ],
+        $version
+    );
 
     if ( should_enqueue_generic_content_assets() ) {
-        wp_enqueue_style( 'aznet-theme-generic-content', get_theme_file_uri( '/assets/css/components/generic-content.css' ), [ 'aznet-theme-tokens' ], $version );
+        wp_enqueue_style(
+            'aznet-theme-generic-content',
+            get_theme_file_uri( '/assets/css/components/generic-content.css' ),
+            [ 'aznet-theme-tokens' ],
+            $version
+        );
     }
 
     if ( should_enqueue_woocommerce_product_assets() ) {
-        wp_enqueue_style( 'aznet-theme-woocommerce-product', get_theme_file_uri( '/assets/css/components/woocommerce-product.css' ), [ 'aznet-theme-tokens' ], $version );
+        wp_enqueue_style(
+            'aznet-theme-woocommerce-product',
+            get_theme_file_uri( '/assets/css/components/woocommerce-product.css' ),
+            [ 'aznet-theme-tokens' ],
+            $version
+        );
     }
 }
