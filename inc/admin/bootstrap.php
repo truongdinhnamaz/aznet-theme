6bb6a1d9bb8af59fb457b2e5b5d3eaae7f92ae43
truongdinhnamaz <truongnam151@gmail.com>
2026-09-03 18:33:14 +0700

    feat: enqueue Control Center assets on its screen

--- inc/admin/bootstrap.php
+++ inc/admin/bootstrap.php
@@ -16,6 +16,7 @@ require_once __DIR__ . '/settings.php';
 
 function bootstrap(): void {
     add_action( 'admin_menu', '\\AZnet\\Theme\\Admin\\ControlCenter\\register_menu' );
+    add_action( 'admin_enqueue_scripts', '\\AZnet\\Theme\\Admin\\ControlCenter\\enqueue_assets' );
     add_action( 'admin_post_aznet_theme_save_u0_settings', '\\AZnet\\Theme\\Admin\\Settings\\handle_save' );
     add_action( 'admin_post_aznet_theme_reset_settings', '\\AZnet\\Theme\\Admin\\Settings\\handle_reset' );
 }
