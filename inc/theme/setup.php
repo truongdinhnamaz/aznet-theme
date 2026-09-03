bedc61af6401cb1ed387be3c242139d585b0800b
truongdinhnamaz <truongnam151@gmail.com>
2026-09-03 19:28:51 +0700

    fix: enable native custom logo workflow

--- inc/theme/setup.php
+++ inc/theme/setup.php
@@ -16,6 +16,7 @@ function setup(): void {
 
     add_theme_support( 'title-tag' );
     add_theme_support( 'post-thumbnails' );
+    add_theme_support( 'custom-logo' );
     add_theme_support( 'responsive-embeds' );
     add_theme_support( 'wp-block-styles' );
     add_theme_support(
