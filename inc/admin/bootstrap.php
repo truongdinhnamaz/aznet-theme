fe3d5275de58e3e1539048b43716ef9beb1d8ee5
truongdinhnamaz <truongnam151@gmail.com>
2026-09-03 18:31:27 +0700

    feat: wire secure Control Center actions

--- inc/admin/bootstrap.php
+++ inc/admin/bootstrap.php
@@ -12,7 +12,10 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once __DIR__ . '/control-center.php';
+require_once __DIR__ . '/settings.php';
 
 function bootstrap(): void {
     add_action( 'admin_menu', '\\AZnet\\Theme\\Admin\\ControlCenter\\register_menu' );
+    add_action( 'admin_post_aznet_theme_save_u0_settings', '\\AZnet\\Theme\\Admin\\Settings\\handle_save' );
+    add_action( 'admin_post_aznet_theme_reset_settings', '\\AZnet\\Theme\\Admin\\Settings\\handle_reset' );
 }
