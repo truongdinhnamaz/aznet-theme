cae0c3a1d60acf685731fcd5f70ced4331fd2e19
truongdinhnamaz <truongnam151@gmail.com>
2026-09-03 13:10:24 +0700

    feat: load Checkout presentation helper

--- inc/theme/bootstrap.php
+++ inc/theme/bootstrap.php
@@ -16,6 +16,7 @@ require_once __DIR__ . '/../integrations/woocommerce.php';
 require_once __DIR__ . '/woocommerce-product.php';
 require_once __DIR__ . '/woocommerce-archive.php';
 require_once __DIR__ . '/woocommerce-cart.php';
+require_once __DIR__ . '/woocommerce-checkout.php';
 require_once __DIR__ . '/content-shell.php';
 require_once __DIR__ . '/assets.php';
 require_once __DIR__ . '/../integrations/rootprofile.php';
