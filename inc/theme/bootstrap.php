978840a14adc6a2490e97e642a3946cb4ace543b
truongdinhnamaz <truongnam151@gmail.com>
2026-09-07 20:50:34 +0700

    feat: wire visual preset presentation hooks

--- inc/theme/bootstrap.php
+++ inc/theme/bootstrap.php
@@ -12,6 +12,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 require_once __DIR__ . '/settings.php';
+require_once __DIR__ . '/design-system.php';
 require_once __DIR__ . '/setup.php';
 require_once __DIR__ . '/../integrations/woocommerce.php';
 require_once __DIR__ . '/woocommerce-product.php';
@@ -28,3 +29,5 @@ require_once __DIR__ . '/rootprofile-current-surface.php';
 
 add_action( 'after_setup_theme', __NAMESPACE__ . '\\setup' );
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\\enqueue_assets' );
+add_action( 'enqueue_block_editor_assets', __NAMESPACE__ . '\\enqueue_editor_design_system_assets' );
+add_filter( 'body_class', __NAMESPACE__ . '\\visual_preset_body_classes' );
