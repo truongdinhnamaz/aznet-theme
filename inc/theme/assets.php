72cbc8286147741c688e828f02c1ba55a79d0211
truongdinhnamaz <truongnam151@gmail.com>
2026-09-08 01:09:06 +0700

    fix: fail soft when Woo Blocks helper is unavailable

--- inc/theme/assets.php
+++ inc/theme/assets.php
@@ -166,7 +166,7 @@ function enqueue_assets(): void {
         );
     }
 
-    if ( should_enqueue_woocommerce_blocks_assets() ) {
+    if ( function_exists( __NAMESPACE__ . '\\should_enqueue_woocommerce_blocks_assets' ) && should_enqueue_woocommerce_blocks_assets() ) {
         wp_enqueue_style(
             'aznet-theme-woocommerce-blocks',
             get_theme_file_uri( '/assets/css/components/woocommerce-blocks.css' ),
