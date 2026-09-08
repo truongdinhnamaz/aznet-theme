65fe84f4b438594a2f81a3d3b74012fd500fffa5
truongdinhnamaz <truongnam151@gmail.com>
2026-09-08 12:44:49 +0700

    fix: avoid empty author links in article meta

--- template-parts/content/meta.php
+++ template-parts/content/meta.php
@@ -10,16 +10,19 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 $author_id      = (int) get_the_author_meta( 'ID' );
-$author_url     = get_author_posts_url( $author_id );
+$author_name    = trim( (string) get_the_author() );
+$author_url     = $author_id > 0 && '' !== $author_name ? get_author_posts_url( $author_id ) : '';
 $published_unix = (int) get_the_time( 'U' );
 $modified_unix  = (int) get_the_modified_time( 'U' );
 $show_modified  = $modified_unix > $published_unix;
 ?>
 <div class="aznet-theme-article__meta">
-    <span class="aznet-theme-article__meta-item aznet-theme-article__author-link">
-        <span class="screen-reader-text"><?php esc_html_e( 'Author:', 'aznet-theme' ); ?></span>
-        <a href="<?php echo esc_url( $author_url ); ?>"><?php echo esc_html( get_the_author() ); ?></a>
-    </span>
+    <?php if ( '' !== $author_url ) : ?>
+        <span class="aznet-theme-article__meta-item aznet-theme-article__author-link">
+            <span class="screen-reader-text"><?php esc_html_e( 'Author:', 'aznet-theme' ); ?></span>
+            <a href="<?php echo esc_url( $author_url ); ?>"><?php echo esc_html( $author_name ); ?></a>
+        </span>
+    <?php endif; ?>
 
     <span class="aznet-theme-article__meta-item">
         <span class="screen-reader-text"><?php esc_html_e( 'Published:', 'aznet-theme' ); ?></span>
