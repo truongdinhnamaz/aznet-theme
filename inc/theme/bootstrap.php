a4eb02296af74afbc37faf49edcea0d7c7b812d5
truongdinhnamaz <truongnam151@gmail.com>
2026-09-17 11:52:30 +0700

    feat: load Y3 footer context

--- inc/theme/bootstrap.php
+++ inc/theme/bootstrap.php
@@ -30,6 +30,7 @@ require_once __DIR__ . '/design-system.php';
 require_once __DIR__ . '/setup.php';
 require_once __DIR__ . '/patterns.php';
 require_once __DIR__ . '/header.php';
+require_once __DIR__ . '/footer.php';
 require_once __DIR__ . '/../integrations/woocommerce.php';
 require_once __DIR__ . '/woocommerce-presentation.php';
 require_once __DIR__ . '/woocommerce-product.php';
