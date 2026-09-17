0ecf1672e868465fc0de2e25f2eb57b02548ade7
truongdinhnamaz <truongnam151@gmail.com>
2026-09-17 10:07:06 +0700

    feat: add native Landing Page template

---
+++ page-templates/landing.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Template Name: Landing
+ * Template Post Type: page
+ *
+ * @package AZnetTheme
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+get_header();
+?>
+<main id="main" class="aznet-theme-main">
+    <?php while ( have_posts() ) : the_post(); ?>
+        <?php get_template_part( 'template-parts/content/page' ); ?>
+    <?php endwhile; ?>
+</main>
+<?php
+get_footer();
