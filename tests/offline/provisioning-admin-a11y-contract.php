27bdaeac78ea835ce30ee33ee5612e09e369b231
truongdinhnamaz <truongnam151@gmail.com>
2026-09-18 03:09:40 +0700

    test: fix provisioning preview RED contract syntax

--- tests/offline/provisioning-admin-a11y-contract.php
+++ tests/offline/provisioning-admin-a11y-contract.php
@@ -24,8 +24,8 @@ foreach ([
     'Tôi đã xem các thay đổi trên và đồng ý áp dụng.',
     'name="confirm_plan"',
     'function provisioning_preview_operation( array $operation, array $plan ): array',
-    "provisioning_find_owned_role( (string) ( $plan['blueprint'] ?? '' ), 'post'",
-    "provisioning_find_owned_role( (string) ( $plan['blueprint'] ?? '' ), 'attachment'",
+    'provisioning_find_owned_role( (string) ( $plan[\'blueprint\'] ?? \'\' ), \'post\'',
+    'provisioning_find_owned_role( (string) ( $plan[\'blueprint\'] ?? \'\' ), \'attachment\'',
     'REUSE starter Post #',
     'REUSE starter media #',
 ] as $needle) {
