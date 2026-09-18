7db436b316ef5a9f9264fa9dd71f6cfad4d7ad80
truongdinhnamaz <truongnam151@gmail.com>
2026-09-18 21:03:03 +0700

    release: promote runtime version to 1.3.6

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'AZNET_THEME_VERSION' ) ) {
-    define( 'AZNET_THEME_VERSION', '1.3.5' );
+    define( 'AZNET_THEME_VERSION', '1.3.6' );
 }
 
 require_once __DIR__ . '/inc/theme/bootstrap.php';
