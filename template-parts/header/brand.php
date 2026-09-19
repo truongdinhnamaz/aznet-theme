7e03ed5deeb0c9e02d76dc2d44db34a9bb53a81b
truongdinhnamaz <truongnam151@gmail.com>
2026-09-19 17:32:12 +0700

    feat: add Law01 brand presentation lockup

--- template-parts/header/brand.php
+++ template-parts/header/brand.php
@@ -11,12 +11,18 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 $site_title = isset( $args['site_title'] ) ? (string) $args['site_title'] : '';
 $home_url   = isset( $args['home_url'] ) ? (string) $args['home_url'] : home_url( '/' );
-$logo_html  = isset( $args['logo_html'] ) ? (string) $args['logo_html'] : '';
+$logo_html       = isset( $args['logo_html'] ) ? (string) $args['logo_html'] : '';
+$law01_homepage   = ! empty( $args['law01_homepage'] );
 ?>
 <a class="aznet-theme-site-header__brand" href="<?php echo esc_url( $home_url ); ?>" rel="home" aria-label="<?php echo esc_attr( $site_title ); ?>">
     <?php if ( '' !== $logo_html ) : ?>
         <?php echo $logo_html; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped -- WordPress attachment HTML from wp_get_attachment_image(). ?>
-        <?php if ( '' !== $site_title ) : ?><span class="aznet-theme-site-header__brand-title"><?php echo esc_html( $site_title ); ?></span><?php endif; ?>
+        <?php if ( '' !== $site_title ) : ?>
+            <span class="aznet-theme-site-header__brand-copy">
+                <span class="aznet-theme-site-header__brand-title"><?php echo esc_html( $site_title ); ?></span>
+                <?php if ( $law01_homepage ) : ?><span class="aznet-theme-site-header__brand-kicker"><?php esc_html_e( 'Văn phòng luật sư', 'aznet-theme' ); ?></span><?php endif; ?>
+            </span>
+        <?php endif; ?>
     <?php else : ?>
         <span><?php echo esc_html( $site_title ); ?></span>
     <?php endif; ?>
