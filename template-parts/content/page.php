6abd48e3c3197eca0ac953a22f1ff36d49d3084f
truongdinhnamaz <truongnam151@gmail.com>
2026-09-21 07:49:02 +0700

    feat: route mapped Services Page to premium presentation

--- template-parts/content/page.php
+++ template-parts/content/page.php
@@ -14,6 +14,7 @@ $crumbs  = \AZnet\Theme\page_breadcrumb_items( (int) get_the_ID() );
 $excerpt = \AZnet\Theme\page_excerpt( (int) get_the_ID() );
 $is_service_detail = \AZnet\Theme\service_page_is_detail( (int) get_the_ID() );
 $is_contact_page = \AZnet\Theme\contact_page_presentation_active( (int) get_the_ID() );
+$is_services_page = \AZnet\Theme\services_page_presentation_active( (int) get_the_ID() );
 $service_contact_url = $is_service_detail ? \AZnet\Theme\service_page_contact_url() : '';
 $service_siblings = $is_service_detail ? \AZnet\Theme\service_page_siblings( (int) get_the_ID() ) : [];
 $page_classes = 'aznet-theme-page aznet-theme-page--' . $variant;
@@ -23,12 +24,25 @@ if ( $is_service_detail ) {
 if ( $is_contact_page ) {
     $page_classes .= ' aznet-theme-page--contact';
 }
+if ( $is_services_page ) {
+    $page_classes .= ' aznet-theme-page--services-root';
+}
 ?>
 <article id="post-<?php the_ID(); ?>" <?php post_class( $page_classes ); ?>>
     <?php if ( $is_contact_page ) : ?>
         <?php
         get_template_part(
             'template-parts/contact/page',
+            null,
+            [
+                'excerpt' => $excerpt,
+            ]
+        );
+        ?>
+    <?php elseif ( $is_services_page ) : ?>
+        <?php
+        get_template_part(
+            'template-parts/services/page',
             null,
             [
                 'excerpt' => $excerpt,
