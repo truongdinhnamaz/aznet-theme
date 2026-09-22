097e6d318c6e90aeab347628bb186b3515504166
truongdinhnamaz <truongnam151@gmail.com>
2026-09-22 18:05:16 +0700

    fix: isolate Curtain 01 process source setting

--- template-parts/homepage/curtain-01/process.php
+++ template-parts/homepage/curtain-01/process.php
@@ -11,7 +11,7 @@ if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-$page = homepage_page_reference( (int) setting( 'homepage_process_page', 0 ) );
+$page = homepage_page_reference( (int) setting( 'homepage_curtain01_process_page', 0 ) );
 if ( ! $page instanceof \WP_Post ) {
     return;
 }
