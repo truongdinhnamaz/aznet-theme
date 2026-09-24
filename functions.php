d0891c9019a9140c743dc1aa3ea542572498e355
truongdinhnamaz <truongnam151@gmail.com>
2026-09-24 16:12:05 +0700

    chore: sync AZnet Theme runtime version 1.3.40

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'AZNET_THEME_VERSION' ) ) {
-    define( 'AZNET_THEME_VERSION', '1.3.39' );
+    define( 'AZNET_THEME_VERSION', '1.3.40' );
 }
 
 require_once __DIR__ . '/inc/theme/bootstrap.php';
