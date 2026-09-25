8ec2afdfd631b07baec1c16666edca6e61a50d73
truongdinhnamaz <truongnam151@gmail.com>
2026-09-25 11:09:22 +0700

    chore: sync Hero source parity runtime version 1.3.48

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'AZNET_THEME_VERSION' ) ) {
-    define( 'AZNET_THEME_VERSION', '1.3.47' );
+    define( 'AZNET_THEME_VERSION', '1.3.48' );
 }
 
 require_once __DIR__ . '/inc/theme/bootstrap.php';
