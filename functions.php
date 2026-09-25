dcb61829a6b15142cbe8adce5dcdc917dcef98c2
truongdinhnamaz <truongnam151@gmail.com>
2026-09-25 11:00:00 +0700

    chore: sync immediate Team publish runtime version 1.3.47

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'AZNET_THEME_VERSION' ) ) {
-    define( 'AZNET_THEME_VERSION', '1.3.46' );
+    define( 'AZNET_THEME_VERSION', '1.3.47' );
 }
 
 require_once __DIR__ . '/inc/theme/bootstrap.php';
