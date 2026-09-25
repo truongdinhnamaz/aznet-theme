28cbbee1633659a3ab79b4e1c921d9dc93aa5c5b
truongdinhnamaz <truongnam151@gmail.com>
2026-09-25 11:49:42 +0700

    chore: sync Law 01 source parity runtime version 1.3.52

--- functions.php
+++ functions.php
@@ -10,7 +10,7 @@ if ( ! defined( 'ABSPATH' ) ) {
 }
 
 if ( ! defined( 'AZNET_THEME_VERSION' ) ) {
-    define( 'AZNET_THEME_VERSION', '1.3.51' );
+    define( 'AZNET_THEME_VERSION', '1.3.52' );
 }
 
 require_once __DIR__ . '/inc/theme/bootstrap.php';
